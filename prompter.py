b58c9f472f98aa6adc5b5f89128af00aba41312e
dpeters19 <dpeters19@bayschoolsf.org>
2017-05-05 12:24:22 -0700

    Fix documentation in prompter
    Added "and verifies it" to the documentation
    Removed completed TODO

--- prompter.py
+++ prompter.py
@@ -2,7 +2,7 @@ import science_utils
 
 
 def prompt_for_answer(question=""):
-    """ Prompts the user for an answer
+    """ Prompts the user for an answer and verifies it
     Args:
         question (String): The question to be displayed to the user
         
@@ -10,7 +10,6 @@ def prompt_for_answer(question=""):
         String: The user's answer
     """
 
-    # TODO Change to verify answer here
     is_acceptable = False
     while not is_acceptable:
         response = input(question)
