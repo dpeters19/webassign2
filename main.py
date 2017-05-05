51aceab0ca81d093ee03acd752b3eee05f2dbd8f
dpeters19 <dpeters19@bayschoolsf.org>
2017-05-05 13:25:27 -0700

    Many things

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-import science_utils
 import prompter
+import science_utils
 
-correct_answer = 100    # TODO change this
+correct_answer = 100  # TODO change this
 correct = False
 number_of_submissions = 0
 MAX_SUBMISSIONS = 3
 
-
-while not correct:
+while not correct and number_of_submissions < MAX_SUBMISSIONS:
     response = prompter.prompt_for_answer("Your answer is: ")
     number_of_submissions += 1
-    if science_utils.is_hyper_scientific(response) and science_utils.is_acceptable_answer(correct_answer, response):
+
+    # Checks if answer is right but notation is wrong
+    if not science_utils.is_hyper_scientific(response):
+
+        # Determines if the answer is numerically correct
+        try:
+            if science_utils.is_acceptable_answer(correct_answer,
+                                                  science_utils.convert_to_scientific_notation(float(response))):
+                print("Your answer is right, but your notation is wrong. \nPlease try again.")
+
+            else:
+                # The answer is not numerically correct
+                print("Hmm. Try again.")
+
+            continue
+        except ValueError as ve:
+            print("That was not a number. Please try again")
+            continue
+
+    if science_utils.is_acceptable_answer(correct_answer, response):
         # If response is close enough but not perfect
         if response != science_utils.convert_to_scientific_notation(correct_answer):
             print("You were close enough.")
@@ -21,5 +39,5 @@ while not correct:
 
         correct = True
 
-    else:
-        print("Hmm. Try again. It might just be a matter of your notation.\n")
+if number_of_submissions == MAX_SUBMISSIONS:
+    print("Make a note that you need help answering this type of question.")
