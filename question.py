2f2efe59285896396de318a82c5d64152964fc77
dpeters19 <dpeters19@bayschoolsf.org>
2017-05-10 10:16:19 -0700

    Tweak question and documentation

--- question.py
+++ question.py
@@ -1,68 +1,78 @@
 import random
+
 import science_utils
 
 
-last_line = '\n'
-question = ""
+def get_question():
+    """ Prompts the user to create a question
+    Args:
+        No parameters
+    Returns:
+        list: The list of lines to print
+        """
+    last_line = '\n'
+    question = ""
 
-print("Type in your question here:")
-while last_line != "":
-    current_line = input()
-    if current_line == "":
-        break
-    question += current_line + "\n"
-
-variables = {}
-
-rand = random.randint
-
-functions = {"rand": rand}
-
-array_of_lines = question.split("\n")
-
-QUESTION_IS_PRESENT = False
-
-for line in array_of_lines:
-    if "QUESTION" in line:
-        QUESTION_IS_PRESENT = True
-        break
-
-if QUESTION_IS_PRESENT:
-    variable_lines = []
-    question_lines = []
-
-    for index in range(len(array_of_lines)):
-        if array_of_lines[index] == "QUESTION":
-            QUESTION_INDEX = index
+    print("Type in your question here:")
+    while last_line != "":
+        current_line = input()
+        if current_line == "":
             break
+        question += current_line + "\n"
 
-    for index in range(QUESTION_INDEX):
-        variable_lines.append(array_of_lines[index])
+    variables = {}
 
-    for index in range(QUESTION_INDEX + 1, len(array_of_lines)):
-        question_lines.append(array_of_lines[index])
+    # Special syntax variables
+    rand = random.randint
 
-    for line in variable_lines:
-        variable, value = line.split("=")
-        variables["$" + variable] = str(eval(value)).replace("_", " ")
+    array_of_lines = question.split("\n")
 
-    for line in question_lines:
-        question_lines[question_lines.index(line)] = science_utils.multiple_replace(line, variables)
-
-    for line in question_lines:
-        print(line)
-
-# This is the backup method, for when there is no QUESTION line found
-else:
-    # Calculate values
-    for word in array_of_lines:
-        if "=" in word:
-            variable, value = word.split("=")
-            variables["$" + variable] = str(eval(value)).replace("_", " ")
-
-    for words in array_of_lines:
-        array_of_lines[array_of_lines.index(words)] = multiple_replace(words, variables)
+    question_is_present = False
 
     for line in array_of_lines:
-        if "=" not in line:
-            print(line)
+        if "QUESTION" in line:
+            question_is_present = True
+            break
+
+    if question_is_present:
+        variable_lines = []
+        question_lines = []
+
+        for index in range(len(array_of_lines)):
+            if array_of_lines[index] == "QUESTION":
+                question_index = index
+                break
+
+        for index in range(question_index):
+            variable_lines.append(array_of_lines[index])
+
+        for index in range(question_index + 1, len(array_of_lines)):
+            question_lines.append(array_of_lines[index])
+
+        for line in variable_lines:
+            variable, value = line.split("=")
+            variables["$" + variable] = str(eval(value)).replace("_", " ")
+
+        for line in question_lines:
+            question_lines[question_lines.index(line)] = science_utils.multiple_replace(line, variables)
+
+        return question_lines
+
+    # This is the backup method, for when there is no QUESTION line found
+    else:
+        # Calculate values
+        for word in array_of_lines:
+            if "=" in word:
+                variable, value = word.split("=")
+                variables["$" + variable] = str(eval(value)).replace("_", " ")
+
+        for words in array_of_lines:
+            array_of_lines[array_of_lines.index(words)] = science_utils.multiple_replace(words, variables)
+
+        to_return = []
+        for line in array_of_lines:
+            if "=" not in line:
+                to_return.append(line)
+
+
+print(get_question())
