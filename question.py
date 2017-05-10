dfb3922196b237158f0b5de9a9f1c9f7aa2c5ddf
dpeters19 <dpeters19@bayschoolsf.org>
2017-05-10 09:25:06 -0700

    Question now works

--- question.py
+++ question.py
@@ -17,29 +17,57 @@ while last_line != "":
         break
     question += current_line + "\n"
 
-
-variable_lines = []
-question_lines = []
-
 variables = {}
 
 rand = random.randint
 
 functions = {"rand": rand}
 
-# This is the backup method, for when there is no QUESTION line found
-
 array_of_lines = question.split("\n")
 
-# Calculate values
-for word in array_of_lines:
-    if "=" in word:
-        variable, value = word.split("=")
+QUESTION_IS_PRESENT = False
+
+for line in array_of_lines:
+    if "QUESTION" in line:
+        QUESTION_IS_PRESENT = True
+        break
+
+if QUESTION_IS_PRESENT:
+    variable_lines = []
+    question_lines = []
+
+    for index in range(len(array_of_lines)):
+        if array_of_lines[index] == "QUESTION":
+            QUESTION_INDEX = index
+            break
+
+    for index in range(QUESTION_INDEX):
+        variable_lines.append(array_of_lines[index])
+
+    for index in range(QUESTION_INDEX + 1, len(array_of_lines)):
+        question_lines.append(array_of_lines[index])
+
+    for line in variable_lines:
+        variable, value = line.split("=")
         variables["$" + variable] = str(eval(value)).replace("_", " ")
 
-for words in array_of_lines:
-    array_of_lines[array_of_lines.index(words)] = multiple_replace(words, variables)
+    for line in question_lines:
+        question_lines[question_lines.index(line)] = multiple_replace(line, variables)
 
-for words in array_of_lines:
-    # TODO find a way to print array_of_lines
-    exec("print(" + words + ")")
+    for line in question_lines:
+        print(line)
+
+# This is the backup method, for when there is no QUESTION line found
+else:
+    # Calculate values
+    for word in array_of_lines:
+        if "=" in word:
+            variable, value = word.split("=")
+            variables["$" + variable] = str(eval(value)).replace("_", " ")
+
+    for words in array_of_lines:
+        array_of_lines[array_of_lines.index(words)] = multiple_replace(words, variables)
+
+    for line in array_of_lines:
+        if "=" not in line:
+            print(line)
